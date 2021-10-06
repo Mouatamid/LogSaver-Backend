515b62ceccef8abd376afaaab21a6e6abc4e6bcc
Mouatamid <mouatamid.ela@gmail.com>
2021-10-06 17:19:05 +0100

    first commit

---
+++ src/main/java/com/mouatamid/logsaver/LogsaverApplication.java
@@ -0,0 +1,13 @@
+package com.mouatamid.logsaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogsaverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mouatamid/logsaver/LogsaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouatamid.logsaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
