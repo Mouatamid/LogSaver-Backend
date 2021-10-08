398217478a333622b39acea55acda369c1a4a5e0
Mouatamid <mouatamid.ela@gmail.com>
2021-10-08 11:45:19 +0100

    Web

--- src/main/java/com/mouatamid/logsaver/LogsaverApplication.java
+++ src/main/java/com/mouatamid/logsaver/LogsaverApplication.java
@@ -1,9 +1,14 @@
 package com.mouatamid.logsaver;
 
+import com.mouatamid.logsaver.services.IAppUserService;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class LogsaverApplication {
@@ -13,8 +18,21 @@ public class LogsaverApplication {
     }
 
     @Bean
+    CommandLineRunner run(IAppUserService service){
+        return args -> {
+            service.saveUser("mouatamid@gmail.com","123456789");
+            service.createLog("mouatamid@gmail.com",new Date(),new Date(4562),"New Log");
+        };
+    }
+
+    @Bean
     ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
---
+++ src/main/java/com/mouatamid/logsaver/web/AppUserController.java
@@ -0,0 +1,43 @@
+package com.mouatamid.logsaver.web;
+
+import com.mouatamid.logsaver.responseModels.AppUserResponseModel;
+import com.mouatamid.logsaver.responseModels.LogResponseModel;
+import com.mouatamid.logsaver.services.IAppUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class AppUserController {
+    private final IAppUserService appUserService;
+
+    @PostMapping("/create")
+    ResponseEntity<AppUserResponseModel> createUser(@RequestParam String email, @RequestParam String password){
+        AppUserResponseModel appUserResponseModel = appUserService.saveUser(email,password);
+        if (appUserResponseModel != null){
+            return new ResponseEntity<>(appUserResponseModel, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/logs")
+    List<LogResponseModel> listLogs(@RequestParam String username){
+        return appUserService.listLogs(username);
+    }
+
+    @PostMapping("/savelog")
+    ResponseEntity<LogResponseModel> saveLog(@RequestParam String username, @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate, @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate, @RequestParam String description){
+        LogResponseModel logResponseModel = appUserService.createLog(username, startDate, endDate, description);
+        if (logResponseModel != null){
+            return new ResponseEntity<>(logResponseModel, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+}
